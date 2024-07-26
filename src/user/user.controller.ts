5b0f31e25af8d843a291c81cc9fc9779b2dda6e9
davidGBbr <davidgomesbrigido@gmail.com>
2024-07-26 10:07:31 -0300

    list users and get user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('users')
 export class UserController {
   private readonly users = [];
+
   @Post()
   async createUser(@Body() body) {
     const { name, email, password } = body;
     const user = { id: Date.now(), name, email, password };
     this.users.push(user);
     return user;
+  }
+
+  @Get()
+  async getUsers() {
+    return this.users;
+  }
+
+  @Get(':id')
+  async getUserById(@Param('id') id: string) {
+    return this.users.find((user) => user.id === Number(id));
   }
 }
