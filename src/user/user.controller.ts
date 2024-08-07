0399bf02cd2cc789f67026787e07d02b190bf10f
davidGBbr <davidgomesbrigido@gmail.com>
2024-08-07 14:43:31 -0300

    interceptors

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,12 +9,15 @@ import {
   Put,
   Delete,
   ParseIntPipe,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UpdateUserDTO } from './dto/update-user.dto';
 import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
 import { UserService } from './user.service';
+import { LogInterceptor } from 'src/interceptors/log.interceptor';
 
+@UseInterceptors(LogInterceptor)
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
